1322726189019839d68c2f035a11c33e342c7cf9
Christian Sales <chs1421@jagmail.southalabama.edu>
2016-10-31 10:08:02 -0500

    Update FTP_Client.py

--- FTP_Client.py
+++ FTP_Client.py
@@ -1,39 +1,32 @@
+
 import socket, re, os, glob, gzip, select
 
 
 
 print("Attempting connection to server...")
+s = ""
+
 try:
     s = socket.socket()
     s.connect(('Bills', 21))
-    userInput = input()
-    s.send(userInput.encode())
-    data = s.recv(4086).decode()
-    print(data)
-except socket.gaierror:
+except (socket.gaierror,ConnectionRefusedError):
     print("Connection Failed")
     quit()
 
 print("Connection Successful!")
 connection = True
-"""
 while connection:
-    try:
-        s = socket.socket()
-        s.connect(('Bills', 21))
-        s.send('ls'.encode)
-    except socket.gaierror:
-        print("Connection Failed")
-        connection = False
-
-
-
-  userInput = input()
-  s.send(userInput)
-  userInput = input()
-  if userInput == "ls":
-
-  if userInput == "cd": """
+    print(s.recv(1024).decode())
+    command = input()
+    if command == "":
+        command = " "
+    s.send(command.encode())
+    print('test1')
+    print(s.recv(1024).decode())
+    print('test2')
+    if command == "quit" or "Quit":
+        print('Shutting down...')
+        quit()
 
 
 
