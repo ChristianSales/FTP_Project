bd747491b7566bed8f96b00e3fe803d0e270af73
Christian Sales <chs1421@jagmail.southalabama.edu>
2016-10-31 10:09:44 -0500

    Update FTP_Server.py
    Created the cd, ls, and quit commands

--- FTP_Server.py
+++ FTP_Server.py
@@ -1,27 +1,71 @@
 import socket, re, os, glob, gzip, select
 
 
-connection = True
+s = ''
 mainDir = "D://"
 currentDir = mainDir
 os.chdir(mainDir)
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 21))
-s.listen(1)
-c, addr = s.accept()
-print('Got connection from', addr)
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((socket.gethostname(), 21))
+    s.listen(1)
+    c, addr = s.accept()
+    print('Got connection from', addr)
+
+except:
+    print("connection failed")
+    quit()
+
+connection = True
 """
+trying = True
+password = False
+while trying:
+    while not password:
+        c.send("Please enter a username and password")
+        loginInfo = c.recv(1024)
+        """
+
+
+
+
 while connection:
+    info = ""
 
+    c.send(os.getcwd().encode())
+    print("test1")
+    data = c.recv(1024).decode()
+    print("test2")
+    command = data.split(' ')
+    try:
+        cmd = command[0]
+    except IndexError:
+        cmd = ""
+        print('test3')
+    try:
+        wish = command[1]
+    except IndexError:
+        wish = ""
+    if cmd == "ls":
+        if len(wish) == 0:
+            for length in range(len(os.listdir(currentDir))):
+                info += (os.listdir(currentDir)[length] + " \n")
+        else:
+            info = "Please enter a valid command"
 
-    s.send(os.getcwd().encode) """
-data = c.recv(1024).decode()
-info = ""
-if data == "ls":
-        for length in range(len(os.listdir(currentDir))):
-            info += (os.listdir(currentDir)[length] + " \n")
-print(info)
-c.send(info.encode())
-   # if userInput == "cd":
-
-
+    elif cmd == "cd":
+        if len(wish) > 0:
+            try:
+                print("test4")
+                os.chdir(wish)
+                currentDir = os.curdir
+                info = " "
+            except FileNotFoundError:
+                info = "This file does not exist"
+        else:
+            info = "Please enter a directory path"
+    elif cmd == "quit" or "Quit":
+        quit()
+    else:
+        info = "Invalid Command"
+    c.send(info.encode())
