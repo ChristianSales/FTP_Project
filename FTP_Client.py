32a2fd2922bc74aa7dd3f9a5490efb5d81ef3e28
Christian Sales <chs1421@jagmail.southalabama.edu>
2016-11-06 18:25:24 -0600

    Added login

--- FTP_Client.py
+++ FTP_Client.py
@@ -18,15 +18,16 @@ login = False
 while not login:
     print("FTP Login 192.168.1.69")
     print("Please enter your username")
-    username = input()
-    password = getpass.win_getpass('Enter Password:')
-    check1 = re.search("@", password)
-    check2 = re.search(".", password["@":])
-    if check1 is True & check2 is True:
+    username = input()                  # getpass library does not work on my IDE
+    print("Please enter your password")    # will create way to hide password by final code due date
+    password = input()
+    check1 = re.search(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", password)
+    if check1 is None or len(username) < 1:      # This regex is taken from EmailRegex.com
+        print("Access Denied")
+
+    else:
         print("Access Granted")
         login = True
-    else:
-        print("Access Denied")
 
 
 
@@ -44,6 +45,9 @@ while connection:
     if command == "quit":
         print('Shutting down...')
         quit()
+
+
+
 
 
 
