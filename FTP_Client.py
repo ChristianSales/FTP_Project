c30a3d7ca6a7cb9e536da9c04b42169bf328669d
Christian Sales <chs1421@jagmail.southalabama.edu>
2016-12-01 01:19:37 -0600

    comments added

--- FTP_Client.py
+++ FTP_Client.py
@@ -1,14 +1,14 @@
 import socket, re, os, glob, gzip, select, getpass
 import time
 
-# Code by Sonny Rasavong and Hunter Sales
+# Code by Sunny Rasavong and Hunter Sales
 
 print("Attempting connection to server...")
 s = ""
 
 try:
     s = socket.socket()
-    s.connect(('Bills', 21))
+    s.connect(('SuN', 21))    # creates a socket and connects to host
 except (socket.gaierror, ConnectionRefusedError):
     print("Connection Failed")
     quit()
@@ -18,33 +18,28 @@ connection = True
 login = False
 
 while not login:
-    print("FTP Login 192.168.1.69")
+    print("FTP Login 10.20.121.127")    # Asks for information for logging in
     print("Please enter your username")
     username = input()                  # getpass library does not work on my IDE
-    print("Please enter your password")    # will create way to hide password by final code due date
+    print("Please enter your password")
     password = input()
     check1 = re.search(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", password)
     if check1 is None or len(username) < 1:      # This regex is taken from EmailRegex.com
-        print("Access Denied")
-
+        print("Access Denied")                   # Checks for proper email format
     else:
         print("Access Granted")
         login = True
-
-
-os.chdir("D://FTP_Client")
-
-
+os.chdir("D://FTP_Client")    # Set directory to FTP_Client
 def put(wish):
     try:
-        with open(wish, 'rb') as inf:
+        with open(wish, 'rb') as inf:    # Open file to be sent
             s.send("uploading".encode())
             while True:
-                inf_data = inf.read(4096)
+                inf_data = inf.read(4096)    # read file into variable and send data to server
                 if inf_data == b'':
                     inf_data = "Finished sending)(*&^%$%^*(*&^%$%^&*&^%$%^&*(*&^%$#$%^&*(&^%$#$%^&*"
                     s.send(inf_data.encode())
-                    time.sleep(.1)
+                    time.sleep(.1)    # Once it reaches the end it breaks and closes
                     break
                 time.sleep(.1)
                 print("Uploading...")
@@ -53,18 +48,12 @@ def put(wish):
     except FileNotFoundError:
         s.send("File not found".encode())
         print('File not found')
-
-
-
-
-
-
 while connection:
-    print(s.recv(1024).decode())
+    print(s.recv(1024).decode())    # Gives current directory
     action = input()
     if action == "":
         action = " "
-    inputs = action.split()
+    inputs = action.split()    # takes input string and slices it for further options
     try:
         cmd = inputs[0]
     except IndexError:
@@ -73,50 +62,45 @@ while connection:
         wish = inputs[1]
     except IndexError:
         wish = ""
-
     s.send(action.encode())
-    if cmd == "ls":
-        print(s.recv(1024).decode())
+    if cmd == "ls" or "dir":
+        print(s.recv(1024).decode())    # accepts the list of files in directory
     elif cmd == "cd":
-        print(s.recv(1024).decode())
-
+        print(s.recv(1024).decode())    # Shows what directory changed too
     elif cmd == "get":
-
         data = wish.split("/")
         filename = data[-1]
-        answer = s.recv(4096).decode()
+        answer = s.recv(4096).decode()    # Gets name of file and open it to be written in too
         print(answer)
         if answer != "File not found" and answer != "Please enter a file\n":
             sentData = ''
             f = open(os.curdir + '/' + data[-1], "wb")
             while sentData != b'Finished sending)(*&^%$%^*(*&^%$%^&*&^%$%^&*(*&^%$#$%^&*(&^%$#$%^&*':
-                sentData = s.recv(4096)
+                sentData = s.recv(4096)    # writes into file until end code is hit
                 if sentData != b'Finished sending)(*&^%$%^*(*&^%$%^&*&^%$%^&*(*&^%$#$%^&*(&^%$#$%^&*':
                     f.write(sentData)
                     print("Downloading...")
-            f.close()
+            f.close()    # closes down file when done writing into
             print("File downloaded")
-
         else:
             print("file not found")
     elif cmd == "put":
-        put(wish)
+        put(wish)    # calls put function
     elif cmd == "mput":
         files = action.split(' ')
-        if len(wish) > 0:
+        if len(wish) > 0:    # calls put function multiple times until all requested files have been addressed
             for length in range(len(files)):
                 if files[length] != "mput":
                     put(files[length])
         else:
             print("Please enter files")
             s.send("Please enter files".encode())
-
     elif cmd == "mget":
         files = action.split(' ')
         for length in range(len(files)):
             if files[length] != "mget":
                 answer = s.recv(4096).decode()
-                print(answer)
+                print(answer)    # Runs a for loop and prepares to accept incoming files and write them to files
                 if answer != "File not found" and answer != "Please enter a file\n":
                     sentData = ''
                     filepath = files[length].split('/')
@@ -131,18 +115,8 @@ while connection:
                                 print("Downloading...")
                         f.close()
                         print("File downloaded")
-
-
-
-
-
-
-
-
-
     else:
-        print(s.recv(1024).decode())
-
+        print(s.recv(1024).decode())    # Accepts invalid input information
     if cmd == "quit":
-        print('Shutting down...')
+        print('Shutting down...')   # Shuts it down
         quit()
